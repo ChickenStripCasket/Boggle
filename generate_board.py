4cc9f4b881d333a26126592dc702ab03f2915512
mdaves1 <80137109+mdaves1@users.noreply.github.com>
2023-04-07 15:21:05 -0500

    Implemented the base boggle solver

---
+++ generate_board.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+
+def generate_board(size=4):
+    # Instantiate new array
+    board = [[random.choice(string.ascii_uppercase)
+              for i in range(size)]
+             for j in range(size)]
+    return board
+    # return [['h', 'b', 'w', 'o'],
+    #         ['t', 'g', 'i', 'c'],
+    #         ['w', 'h', 'c', 'm'],
+    #         ['b', 'w', 's', 'u']]
