28e1c82432c27d7d1ca58d5a34dac7f0293c1192
cmoore53 <123041153+cmoore53@users.noreply.github.com>
2023-04-22 15:44:38 -0500

    Fixed letter images and clock is on screen
    Made letter images centered in their rectangles and made the clock countdown on screen rather than in system output

--- build/boggle_board.py
+++ build/boggle_board.py
@@ -7,7 +7,7 @@ from prefix_tree import PrefixTree
 from combinations import Combinations
 
 class Board_Screen():
-    def __init__(self,players=False,setting=1):
+    def __init__(self,players=False,setting=1,size=4):
         pygame.init()
         if(players):
             print("2P")
@@ -20,16 +20,16 @@ class Board_Screen():
                 print("1P H")
         screen_height = 800
         screen_width = 1000
-        self.game_board=generate_board(4)
+        self.game_board=generate_board(size)
         #"C:\\Users\\crmoo\\Documents\\GitHub\\Boggle\\build\\platformerGraphics_gui_text\\Individual\\Upper_A.png"
         #pygame.image.load(self.getLetterImage('a'))
         board_images = []
         i=0
         for x in self.game_board:
             for k in x:
-                board_images.append(pygame.image.load(Board_Screen.getLetterImage(self,k)))
+                board_images.append(pygame.transform.scale(pygame.image.load(Board_Screen.getLetterImage(self,k)),(67,56)))
                 i+=1
-        
+
         entryBox = tkinter.Entry()
 
         pprint(self.game_board)
@@ -51,7 +51,7 @@ class Board_Screen():
         boggle_squares = []
         square_side_length = 150
         count = 0
-        while(count<16):
+        while(count<size*size):
             left = (count%4)*square_side_length+(2*screen_width/3)-(2*square_side_length)
             top = (count//4)*square_side_length+(screen_height/2)-(2*square_side_length)
             boggle_squares.append(pygame.Rect(left,top,square_side_length,square_side_length))
@@ -63,6 +63,7 @@ class Board_Screen():
         timeLimit = 1000*60*3
         pygame.time.set_timer(pygame.QUIT,timeLimit,1)
         while running:
+            screen.fill(background_color)
             #Clock prints time since start every second
             clock.tick(60)
             timeLimit -= clock.get_time()
@@ -70,14 +71,16 @@ class Board_Screen():
             if(secondTimer>1000):
                 secondTimer-=1000
                 print(timeLimit//1000)
-            screen.fill(background_color)
+                clock_text = font.render(str(timeLimit//60000)+":"+str((timeLimit//1000)%60),True,'White')
+            screen.blit(clock_text,((screen_width*2/3)-40,30))
 
-            screen_divider1 = pygame.draw.line(screen, 0, (screen_width/3,0), (screen_width/3,screen_height), 4)
-            screen_divider2 = pygame.draw.line(screen, 0, (0,(screen_height/4)*3), (screen_width/3,(screen_height/4)*3), 4)
+            pygame.draw.line(screen, 0, (screen_width/3,0), (screen_width/3,screen_height), 4)
+            pygame.draw.line(screen, 0, (0,(screen_height/4)*3), (screen_width/3,(screen_height/4)*3), 4)
+
             i=0
             for x in boggle_squares:
                 pygame.draw.rect(screen, 0, x, 2)
-                screen.blit(board_images[i],x)
+                screen.blit(board_images[i],(x.x+40,x.y+40))
                 i+=1
 
             for event in pygame.event.get():
