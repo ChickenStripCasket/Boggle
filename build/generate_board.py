7f5487f1ba40d9ed5a6862d2ee682a1dfeee0f4d
mdaves1 <80137109+mdaves1@users.noreply.github.com>
2023-04-25 20:24:05 -0500

    Fixed Qu rule bug

--- build/generate_board.py
+++ build/generate_board.py
@@ -5,7 +5,7 @@ def generate_board():
     combinations=[['A', 'E', 'A', 'N', 'E', 'G'], ['A' ,'H' ,'S', 'P' ,'C', 'O'],['A', 'S', 'P', 'F', 'F', 'K'],['O', 'B', 'J' ,'O' ,'A', 'B'],
               ['I', 'O', 'T' ,'M', 'U', 'C'], ['R' ,'Y' ,'V', 'D', 'E' ,'L'],['L', 'R', 'E', 'I' ,'X', 'D'],['E' ,'I', 'U' ,'N' ,'E' ,'S'],
               ['W', 'N' ,'G', 'E', 'E' ,'H'],['L', 'N', 'H' ,'N', 'R' ,'Z'],['T' ,'S' ,'T', 'I', 'Y' ,'D'],['O', 'W', 'T', 'O' ,'A', 'T'],
-              ['E' ,'R', 'T', 'T' ,'Y', 'L'],['T' ,'O', 'E', 'S', 'S' ,'I'],['T', 'E', 'R', 'W', 'H', 'V'],['N' ,'U' ,'I', 'H' ,'M' ,'Qu']]
+              ['E' ,'R', 'T', 'T' ,'Y', 'L'],['T' ,'O', 'E', 'S', 'S' ,'I'],['T', 'E', 'R', 'W', 'H', 'V'],['N' ,'U' ,'I', 'H' ,'M' ,'Q']]
     #initialize board
     board=[]
     for i in range(0,len(combinations)):
